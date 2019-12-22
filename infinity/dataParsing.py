0bb70d28d26e2dbeda3c50f07059d87049e13b4d
AlDacMac <alasdairmacgsign@gmail.com>
2019-12-22 18:15:34 +0000

    Split specs into obligs and options, moved getting them into profile init
    Obligs refers to skills and equipment with the obligatory lable, and options to those with optional.

--- infinity/dataParsing.py
+++ infinity/dataParsing.py
@@ -1,7 +1,18 @@
 import json
 
 
-optional = {""}
+optional = {"Shock Immunity", "Immunity: POS", "Total Immunity", "Sixth Sense L1", "Sixth Sense L2", "Surprise Shot L1",
+            "Surprise Shot L2", "Surprise Attack", "Marksmanship L1", "Marksmanship L2", "Poison"}
+
+
+def getArmyUnits(armyname):
+    unit_names = ""
+    with open("infinityStats/" + armyname + "_units.json", "r") as read_file:
+        units = json.load(read_file)
+        for profile in units:
+            if("obsolete" in profile.keys()):
+                continue
+            print(profile["isc"])
 
 
 class Profile:
@@ -9,6 +20,29 @@ class Profile:
         self.army = army.lower()[0:4]
         self.isc = isc
         self.child = child
+        # Unit skills and equipment are divided into those that are obligatory and those that are optional
+        obligs: set = set({})
+        options: set = set({})
+        with open("infinityStats/" + self.army + "_units.json", "r") as read_file:
+            units = json.load(read_file)
+            for profile in units:
+                if ("obsolete" in profile):
+                    continue
+                elif (profile["isc"] == self.isc):
+                    if "spec" in profile:
+                        for spec in profile["spec"]:
+                            if spec in optional:
+                                options.add(spec)
+                            else:
+                                obligs.add(spec)
+                    for child in profile["childs"]:
+                        if child["name"] == self.child:
+                            if "spec" in child:
+                                for spec in child["spec"]:
+                                    if spec in optional:
+                                        options.add(spec)
+                                    else:
+                                        obligs.add(spec)
 
     def getStat(self, stat):
         with open("infinityStats/" + self.army + "_units.json", "r") as read_file:
@@ -20,33 +54,6 @@ class Profile:
                     return profile[stat]
             raise LookupError
 
-    def getunitspec(self):
-        specs: set = set({})
-        with open("infinityStats/" + self.army + "_units.json", "r") as read_file:
-            units = json.load(read_file)
-            for profile in units:
-                if ("obsolete" in profile):
-                    continue
-                elif (profile["isc"] == self.isc):
-                    if "spec" in profile:
-                        for spec in profile["spec"]:
-                            specs.add(spec)
-                    for child in profile["childs"]:
-                        if child["name"] == self.child:
-                            if "spec" in child:
-                                for spec in child["spec"]:
-                                    specs.add(spec)
-        return specs
-
-
-def getArmyUnits(armyname):
-    unit_names = ""
-    with open("infinityStats/" + armyname + "_units.json", "r") as read_file:
-        units = json.load(read_file)
-        for profile in units:
-            if("obsolete" in profile.keys()):
-                continue
-            print(profile["isc"])
 
 
 def getAmmoTypes(weaponname):
