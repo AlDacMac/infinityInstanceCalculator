553f0cce5b738befe9ad8711287d2a93b7a34d24
AlDacMac <alasdairmacgsign@gmail.com>
2020-01-13 11:25:12 +0000

    Lists defining ammo qualities

--- infinity/requestHandling.py
+++ infinity/requestHandling.py
@@ -2,6 +2,23 @@ from infinity.dataParsing import *
 from infinity.diceMaths import *
 from math import *
 
+armAmmo = {"N", "AP", "EXP", "Shock", "DA", "Fire", "K1", "Monofilament", "Plasma", "T2"}
+
+btsAmmo = {"Breaker", "DT", "E/M", "E/M2", "Flash", "Nanotech", "Plasma", "Stun", "Viral", "Jammer", "Sepsitor",
+           "Phermonic"}
+
+phAmmo = {"ADH"}
+
+armWoundAmmo = {"N", "AP", "EXP", "Shock", "DA", "Fire", "K1", "Plasma", "T2"}
+
+btsWoundAmmo = {"Breaker", "DT", "Nanotech", "Viral", "Plasma", "Phermonic"}
+
+btsEffectOnCrit = {"Breaker", "DT", "E/M", "E/M2", "Flash", "Nanotech", "Stun", "Viral", "Jammer", "Sepsitor",
+                   "Phermonic"}
+
+armEffectOnCrit = {"N", "AP", "EXP", "Shock", "DA", "Fire", "K1", "Monofilament", "Plasma", "T2"}
+
+phEffectOnCrit = {"ADH"}
 
 # attributes tells you extra information about the unit, e.g that it's in cover or a low vis zone, it's using surprise
 #   attack, or it is buffed with assisted fire - note that all optional skills are here if they are in use
