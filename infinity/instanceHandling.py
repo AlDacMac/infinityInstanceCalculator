06a951849533f4c02366ad2718b9de27679ada03
AlDacMac <alasdairmacgsign@gmail.com>
2020-06-04 12:54:46 +0100

    Created instanceHandling, added ammo and stuff
    Instance handling will replace requestHandling - I wasn't happy with the layout or design in general

---
+++ infinity/instanceHandling.py
@@ -0,0 +1,22 @@
+from infinity.dataParsing import *
+from infinity.diceMaths import *
+from math import *
+
+armAmmo = {"N", "AP", "EXP", "Shock", "DA", "Fire", "K1", "Monofilament", "Plasma", "T2"}
+
+btsAmmo = {"Breaker", "DT", "E/M", "E/M2", "Flash", "Nanotech", "Plasma", "Stun", "Viral", "Jammer", "Sepsitor",
+           "Phermonic"}
+
+phAmmo = {"ADH"}
+
+armWoundAmmo = {"N", "AP", "EXP", "Shock", "DA", "Fire", "K1", "Plasma", "T2"}
+
+btsWoundAmmo = {"Breaker", "DT", "Nanotech", "Viral", "Plasma", "Phermonic"}
+
+btsEffectOnCrit = {"Breaker", "DT", "E/M", "E/M2", "Flash", "Nanotech", "Stun", "Viral", "Jammer", "Sepsitor",
+                   "Phermonic"}
+
+armEffectOnCrit = {"N", "AP", "EXP", "Shock", "DA", "Fire", "K1", "Monofilament", "Plasma", "T2"}
+
+phEffectOnCrit = {"ADH"}
+
